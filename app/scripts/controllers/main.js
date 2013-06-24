dde6a56944f9ba627fba583e35e79a92fb9c059b
Gerald de Jong <gerald@delving.eu>
2013-06-24 16:59:53 +0200

    Test document tree built, and panels selecting values at each level.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,69 @@
 'use strict';
 
 angular.module('cultureCollectorApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('MainCtrl', ['$scope', function ($scope) {
 
-    $scope.melvins = [
-        'Gerald',
-        'Eric',
-        'Thomas',
-        'Manuel',
-        'Sjoerd',
-        'Juliane'
-    ];
-  }]);
+
+        var doc = {
+            'identifier': 'DOC123',
+            'name': 'Document',
+            'elements': [
+                {
+                    'name': 'Basics',
+                    'elements': [
+                        { 'name': 'Type' },
+                        { 'name': 'Condition' }
+                    ]
+                },
+                {
+                    'name': 'Object',
+                    'elements': [
+                        { 'name': 'Link' },
+                        { 'name': 'MimeType' }
+                    ]
+                },
+                {
+                    'name': 'Source',
+                    'elements': [
+                        { 'name': 'URI' },
+                        { 'name': 'Type' },
+                        { 'name': 'Note' }
+                    ]
+                },
+                {
+                    'name': 'Creation',
+                    'elements': [
+                        { 'name': 'Date' },
+                        { 'name': 'Type' },
+                        {
+                            'name': 'Creator',
+                            'elements': [
+                                { 'name': 'URI' },
+                                { 'name': 'Preferred Label' }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    'name': "Events",
+                    'elements': [
+                        { 'name': 'Link' },
+                        { 'name': 'MimeType' }
+                    ]
+                }
+            ]
+        };
+
+        $scope.choose = function (element, here) {
+            $scope.panels[here + 1] = {
+                'element': element
+            };
+        };
+
+        $scope.panels = [];
+
+        $scope.panels[0] = {
+            'element': doc
+        };
+
+    }]);
