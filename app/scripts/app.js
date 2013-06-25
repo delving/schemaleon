bc8a946f4eb85bc24bed40af77cc526891545ab0
zemyatin <eric@delving.eu>
2013-06-25 17:18:11 +0200

    angular-ui-bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('cultureCollectorApp', [])
+angular.module('cultureCollectorApp', ['ui.bootstrap'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
