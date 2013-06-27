08e902659bbab5f79e525a74bbf49453586e59da
Gerald de Jong <gerald@delving.eu>
2013-06-27 15:51:13 +0200

    test starting to work

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,20 +2,22 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('CultureCollectorApp'));
+    // load the controller's module
+    beforeEach(module('CultureCollectorApp'));
 
-  var MainCtrl, scope;
+    var MainCtrl, scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope, Docs) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope,
+            Docs: Docs
+        });
+    }));
+
+    it('should have panels in the scope', function () {
+        expect(scope.panels.length).toBe(1);
+        expect(scope.panels[0].element.name).toBe('Document')
     });
-  }));
-
-  it('should have panels in the scope', function () {
-    expect(scope.panels.length).toBe(1);
-  });
 });
