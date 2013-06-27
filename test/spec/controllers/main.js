3c71352f705bc1aa4becbb3d426176c66872ff10
Gerald de Jong <gerald@delving.eu>
2013-06-27 13:19:22 +0200

    Added Angular-XML

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,8 +5,7 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('CultureCollectorApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl, scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
