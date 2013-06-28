c725c982234499415ccaff98e7aa7d95cc9630d1
zemyatin <eric@delving.eu>
2013-06-28 12:05:33 +0200

    almost three tests passing

--- test/e2e/scenario.js
+++ test/e2e/scenario.js
@@ -2,15 +2,10 @@
 
 describe('End2End Testing', function () {
 
-
-//    it('should have panels in the scope', function () {
-//        browser().navigateTo('/');
-//        expect(scope.panels.length).toBe(1);
-//        expect(scope.panels[0].element.name).toBe('Gumby')
-//    });
-
     it("should show a panel with 5 elements", function () {
         browser().navigateTo('/#/');
+        var panels = repeater('td.panel').count();
+        expect(panels).toBe(1);
         var links = element('a').count();
         expect(links).toBe(5);
     });
@@ -18,9 +13,18 @@ describe('End2End Testing', function () {
     it("should show a second panel when an element is clicked", function () {
         var firstLink = element('a:first');
         firstLink.click();
-//        expect.
+        var panels = repeater('td.panel').count();
+        expect(panels).toBe(2);
+//        pause();
+    });
+
+    it("should add a new element to the current list of elements", function() {
+       var addSibling = element('span.plus');
+        addSibling.click();
         pause();
     });
+
+
 
 //    it("should add another panel", function () {
 //        element(':a.level1').click();
