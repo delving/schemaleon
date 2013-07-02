f2f6765bef2b9701c956ab39d8065813a38574d4
Gerald de Jong <gerald@delving.eu>
2013-07-02 17:44:10 +0200

    Initial schema experiment.

--- test/spec/services.js
+++ test/spec/services.js
@@ -23,7 +23,14 @@ describe('Service: Docs', function () {
                 elements: [
                     {
                         name: 'type',
-                        value: 'Landscapes'
+                        value: 'Landscapes',
+                        localVocabulary: {
+                            options: [
+                                'Landscapes',
+                                'Portraits',
+                                'Nudes'
+                            ]
+                        }
                     }
                 ]
             },
@@ -47,32 +54,66 @@ describe('Service: Docs', function () {
         ]
     };
 
+    var schemas = [
+        {
+            path: [ 'document', 'basics', 'type' ],
+            insert: {
+                localVocabulary: {
+                    options: [
+                        'Landscapes',
+                        'Portraits',
+                        'Nudes'
+                    ]
+                }
+            }
+        }
+    ];
+
+    var embellish = function (path, to) {
+        function arraysEqual(arr1, arr2) {
+            if (arr1.length !== arr2.length) return false;
+            for (var i = arr1.length; i--;) if (arr1[i] !== arr2[i]) return false;
+            return true;
+        }
+        _.each(schemas, function(member) {
+            if (arraysEqual(path, member.path)) {
+                for (var key in member.insert) {
+                    to[key] = member.insert[key];
+                }
+            }
+        });
+    };
+
     var xmlToDoc = function (xml) {
-        var generate = function (el, doc, path) {
-            for (var key in el) {
+        function generate(from, to, path) {
+            for (var key in from) {
                 if (key == '__cnt' || key.indexOf('_asArray') > 0) continue;
-                var value = el[key];
+                path.push(key);
+                var value = from[key];
                 if (_.isString(value)) {
-                    doc.elements.push({ name: key, value: value });
+                    var stringElement = { name: key, value: value };
+                    to.elements.push(stringElement);
+                    embellish(path, stringElement);
                 }
                 else {
                     if (_.isArray(value)) {
                         for (var i = 0; i < value.length; i++) {
-                            path.push(key);
-                            doc.elements.push({ name: key, value: value[i] });
-                            path.pop();
+                            var arrayElement = { name: key, value: value[i] };
+                            to.elements.push(arrayElement);
+                            embellish(path, arrayElement);
+
                         }
                     }
                     else if (_.isObject(value)) {
                         var subDoc = { name: key, elements: [] };
-                        path.push(key);
                         generate(value, subDoc, path);
-                        path.pop();
-                        doc.elements.push(subDoc);
+                        to.elements.push(subDoc);
+                        embellish(path, subDoc);
                     }
                 }
+                path.pop();
             }
-        };
+        }
         var result = { elements: [] };
         generate(xml, result, []);
         return result.elements[0];
@@ -83,6 +124,8 @@ describe('Service: Docs', function () {
         var result = xmlToDoc(xml);
         var jsonString = JSON.stringify(result);
         var expectedString = JSON.stringify(expected);
+//        console.log(jsonString);
+//        console.log(expectedString);
         expect(jsonString).toBe(expectedString);
     })
 
