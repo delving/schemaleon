3a13e9c7957806d25f1aad9025e415577c5c74c9
Gerald de Jong <gerald@delving.eu>
2013-07-23 17:52:48 +0200

    Finally solved the problem with the basex driver.  Informed the developer.

--- test/server/test-server.js
+++ test/server/test-server.js
@@ -1,12 +1,53 @@
 'use strict';
 
-var data = require('../../app/server/fake-data')
+var basex = require('basex');
+basex.debug_mode = false;
 
-exports.testSomething =  function (test) {
-    // just a trivial test for show
-    test.ok(data.documentTree, "Missing doc tree");
-    test.ok(data.vocabResponse, "Missing vocab response");
-    test.ok(data.docList, "Missing doc list");
-    test.done();
+var session = new basex.Session();
+
+exports.testCreate = function (test) {
+    test.expect(1);
+    session.execute('create db gumby', function (error, reply) {
+        test.ok(reply.ok, 'problem creating database')
+        test.done();
+    });
+};
+
+exports.testAdd = function (test) {
+    test.expect(1);
+    session.add("/people.xml", "<people><person>Gumby\nman</person><something>else</something><person>Pokey\nhorse</person></people>", function (error, reply) {
+        test.ok(reply.ok, 'problem adding');
+        test.done();
+    });
+};
+
+exports.testGoodQuery = function (test) {
+    test.expect(1);
+    var query = session.query('//person');
+    query.results(function (error, reply) {
+        console.log(JSON.stringify(reply));
+        test.ok(true, "made it");
+        query.close();
+        test.done();
+    });
+};
+
+exports.testBadQuery = function (test) {
+    test.expect(1);
+    var query = session.query('//somethingNotThere');
+    query.results(function (error, reply) {
+        console.log(JSON.stringify(reply));
+        test.ok(true, "made it");
+        query.close();
+        test.done();
+    });
+};
+
+exports.testDrop = function (test) {
+    test.expect(1);
+    session.execute('drop db gumby', function (error, reply) {
+        test.ok(reply.ok, 'problem dropping database')
+        test.done();
+    });
 };
 
