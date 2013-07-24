6ca34d7e0c9be7ba2a657b2e7d0c3baa85555cca
Gerald de Jong <gerald@delving.eu>
2013-07-24 13:51:58 +0200

    Now i18n basex test is also setting element title and doc.

--- test/server/test-storage.js
+++ test/server/test-storage.js
@@ -60,6 +60,65 @@ exports.testAfterSetLabelAgain = function (test) {
     });
 };
 
+exports.testSetElementTitle = function (test) {
+    test.expect(1);
+    storage.setElementTitle('en', 'Identifier', "ID00001", function (ok) {
+        test.ok(ok, 'Problem setting element title');
+        test.done();
+    });
+};
+
+exports.testAfterSetElementTitle = function (test) {
+    test.expect(3);
+    storage.getLanguage('en', function (xml) {
+        console.log("fetched:\n" + xml);
+        test.ok(xml, "no xml");
+        test.ok(xml.length == 1, "should be one entry");
+        test.ok(xml[0].indexOf('ID00001') > 0, 'No identifier appears');
+        test.done();
+    });
+};
+
+exports.testSetElementTitleAgain = function (test) {
+    test.expect(1);
+    storage.setElementTitle('en', 'Identifier', "ID00002", function (ok) {
+        test.ok(ok, 'Problem setting element title');
+        test.done();
+    });
+};
+
+exports.testAfterSetElementTitleAgain = function (test) {
+    test.expect(3);
+    storage.getLanguage('en', function (xml) {
+        console.log("fetched:\n" + xml);
+        test.ok(xml, "no xml");
+        test.ok(xml.length == 1, "should be one entry");
+        test.ok(xml[0].indexOf('ID00002') > 0, 'No identifier appears');
+        test.done();
+    });
+};
+
+exports.testSetElementDoc = function (test) {
+    test.expect(1);
+    storage.setElementDoc('en', "Identifier", 'Some nice documentation for you all.', function(ok) {
+        test.ok(ok, 'Problem setting element doc');
+        test.done();
+    });
+};
+
+exports.testAfterSetElementDoc = function (test) {
+    test.expect(4);
+    storage.getLanguage('en', function (xml) {
+        console.log("fetched:\n" + xml);
+        test.ok(xml, "no xml");
+        test.ok(xml.length == 1, "should be one entry");
+        test.ok(xml[0].indexOf('ID00002') > 0, 'No identifier appears');
+        test.ok(xml[0].indexOf('nice documentation') > 0, 'No doc appears');
+        test.done();
+    });
+};
+
+
 exports.dropIt = function (test) {
     test.expect(1);
     storage.session.execute('drop db oscrtest', function (error, reply) {
