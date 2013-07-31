30edf53c2de18b961e677e4684fa0475e2b622eb
Gerald de Jong <gerald@delving.eu>
2013-07-31 16:50:19 +0200

    Recent items appear

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -3,42 +3,49 @@
 var CultureCollectorApp = angular.module('CultureCollectorApp');
 
 CultureCollectorApp.controller('NavigationController',
-    ['$rootScope','$scope', '$location',
+    ['$rootScope', '$scope', '$location',
         function ($rootScope, $scope, $location) {
+
             $scope.mainMenu = {
                 links: [
                     {name: "Dashboard", path: "/dashboard", icon: 'icon-home', active: false},
                     {name: "Documents", path: "/document/", icon: 'icon-th-list', active: false}
-                ],
-                activeLabel: 'Dashboard'
+                ]
             };
-            $scope.choose = function (path) {
-                if($rootScope.config.showTranslationEditor) {
-                    return;
-                }
-                _.forEach($scope.mainMenu.links, function (link) {
-                    link.active = (link.path == path);
-                    if (link.active) {
-                        $scope.mainMenu.activeLabel = link.label;
-                        $location.path(link.path);
-                    }
-                });
-            };
+
             var anyActive = false;
             _.forEach($scope.mainMenu.links, function (link) {
-                var sought = link.path.substring(3);
-                link.active = ($location.path().indexOf(sought) >= 0);
-                if (link.active) {
-                    anyActive = true;
-                    $scope.mainMenu.activeLabel = link.label;
-                }
+                link.active = ($location.path().indexOf(link.path) >= 0);
+                if (link.active) anyActive = true;
             });
             if (!anyActive) {
                 $scope.mainMenu.links[0].active = true;
             }
 
-            $scope.getInclude = function(){
-                if($location.path().indexOf('/object/') >= 0){
+            $scope.choosePath = function (path) {
+                if ($rootScope.config.showTranslationEditor) {
+                    return;
+                }
+                var activeItem = false;
+                _.forEach($scope.mainMenu.links, function (link) {
+                    link.active = (link.path === path);
+                    if (link.active) activeItem = true;
+                });
+                if (!activeItem && path.indexOf('OSCR') > 0) {
+                    var identifier = path.substring(path.lastIndexOf("/") + 1, path.length);
+                    var freshLabel = {
+                        name: identifier,
+                        path: path,
+                        icon: 'icon-th-home',
+                        active: true
+                    };
+                    $scope.mainMenu.links.push(freshLabel);
+                }
+                $location.path(path);
+            };
+
+            $scope.getInclude = function () {
+                if ($location.path().indexOf('/object/') >= 0) {
                     return "views/legend.html";
                 }
                 return "";
