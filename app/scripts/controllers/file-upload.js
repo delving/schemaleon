0c571b226fe88d4a1e050a9e98df64ab79a1598a
zemyatin <eric@delving.eu>
2013-08-05 14:32:48 +0200

    adding some of the example controllers, url pointing to server throws error though (it's pointing to php)

--- app/scripts/controllers/file-upload.js
+++ app/scripts/controllers/file-upload.js
@@ -3,4 +3,72 @@
 
 var CultureCollectorApp = angular.module('CultureCollectorApp');
 
+var url = '/app/components/blueimp-file-upload/server/php/';
+
 // todo FileUploadController
+CultureCollectorApp.config([
+    '$httpProvider', 'fileUploadProvider',
+    function ($httpProvider, fileUploadProvider) {
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        fileUploadProvider.defaults.redirect = window.location.href.replace(
+            /\/[^\/]*$/,
+            '/cors/result.html?%s'
+        );
+    }
+]);
+
+CultureCollectorApp.controller('FileUploadController', [
+    '$scope', '$http', '$filter', '$window',
+    function ($scope, $http) {
+        $scope.options = {
+            url: url
+        };
+
+        $scope.loadingFiles = true;
+        $http.get(url)
+            .then(
+            function (response) {
+                $scope.loadingFiles = false;
+                $scope.queue = response.data.files || [];
+            },
+            function () {
+                $scope.loadingFiles = false;
+            }
+        );
+
+    }
+]);
+
+CultureCollectorApp.controller('FileDestroyController', [
+    '$scope', '$http',
+    function ($scope, $http) {
+        var file = $scope.file,
+            state;
+        if (file.url) {
+            file.$state = function () {
+                return state;
+            };
+            file.$destroy = function () {
+                state = 'pending';
+                return $http({
+                    url: file.deleteUrl,
+                    method: file.deleteType
+                }).then(
+                    function () {
+                        state = 'resolved';
+                        $scope.clear(file);
+                    },
+                    function () {
+                        state = 'rejected';
+                    }
+                );
+            };
+        } else if (!file.$cancel && !file._index) {
+            file.$cancel = function () {
+                $scope.clear(file);
+            };
+        }
+    }
+]);
+
+
