b16558c1d880c93d2fd418ac43c41ca7ab0a3c00
zemyatin <eric@delving.eu>
2013-08-22 12:31:20 +0200

    logged in user info

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -3,11 +3,27 @@ var OSCR = angular.module('OSCR');
 OSCR.controller(
     'DashboardController',
     function ($rootScope, $scope, $location, $cookieStore, Person) {
+
+        // GLOBAL OSCR STATS
         $scope.counts = [
             {type: "Groups", count: 5},
             {type: "Users", count: 32},
             {type: "Documents", count: 324},
             {type: "Media items", count: 200}
         ];
+
+        // LOGGED IN USER STATS
+        $scope.currentUserStats = {
+            username: "Oscar Wilde",
+            memberships: [
+                {groupName: "OSCR", memberSince: "22-08-2013"},
+                {groupName: "Group x", memberSince: "22-08-2013"},
+                {groupName: "Group y", memberSince: "22-08-2013"},
+                {groupName: "Group z", memberSince: "22-08-2013"}
+            ]
+        };
+
+
+
     }
 );
