83616ff4136ee23434d8dab6acbbc488ab6b8de0
zemyatin <eric@delving.eu>
2013-08-27 11:42:23 +0200

    bidprentje  ...

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -76,6 +76,7 @@ OSCR.controller(
             links: [
                 {name: "Dashboard", path: "/dashboard", icon: 'icon-home', active: false},
                 {name: "Photographs", path: "/document/Photograph", icon: 'icon-file', active: false},
+                {name: "Bidprentje", path: "/document/BidPrentje", icon: 'icon-file', active: false},
                 {name: "MediaUpload", path: "/media", icon: 'icon-upload', active: false},
                 {name: "People", path: "/people", icon: 'icon-user', active: false}
             ]
