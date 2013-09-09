7d5e565c144ff6c18a9e49a3c62da5f4500b4b47
zemyatin <eric@delving.eu>
2013-09-09 15:57:39 +0200

    new active icon for icon-users ... rename the People nav element to UserManagement

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -83,7 +83,7 @@ OSCR.controller(
         $scope.mainMenu = {
             links: [
                 {name: "Dashboard", path: "/dashboard", icon: 'icon-home', active: false},
-                {name: "People", path: "/people", icon: 'icon-user', active: false},
+                {name: "UserManagement", path: "/people", icon: 'icon-users', active: false},
                 {name: "MediaUpload", path: "/media", icon: 'icon-upload', active: false},
                 {name: "Photo", path: "/document/Photo", icon: 'icon-file', active: false},
                 {name: "InMemoriam", path: "/document/InMemoriam", icon: 'icon-file', active: false},
