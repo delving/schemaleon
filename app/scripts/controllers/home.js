6e3f7e173dc2028fc8186e5f682b641007029aec
zemyatin <eric@delving.eu>
2014-01-23 18:13:05 +0100

    global search front-end scaffolding

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,12 +2,19 @@ var OSCR = angular.module('OSCR');
 
 OSCR.controller(
     'HomeController',
-    function ($rootScope, $scope, Person) {
+    function ($scope, Person, $location) {
         function getAllGroups() {
             Person.getAllGroups(function (list) {
                 $scope.groupList = list;
             });
         }
         getAllGroups();
+
+        $('#list-current-groups').on('change',function(){
+            var path = $(this).val();
+            console.log(path);
+
+            $scope.$apply( $location.path(path) );
+        });
     }
 );
