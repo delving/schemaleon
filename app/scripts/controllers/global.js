c1196ccc921f863e7bf1f51740d764c68528660a
geralddejong <gerald@delving.eu>
2014-02-13 10:41:23 +0100

    Fixed modal i18n problem.

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -51,7 +51,7 @@ OSCR.controller(
             showTranslationEditor: false
         };
 
-       $rootScope.disableChoosePath = false;
+        $rootScope.disableChoosePath = false;
 
         $rootScope.setDocumentDirty = function(dirty, saveDocument, revertDocument) {
             $rootScope.disableChoosePath = dirty;
